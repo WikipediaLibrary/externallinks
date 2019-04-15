bbf9c4f93c6f0a2b919595a395b6f1ff9c1f75df
Samwalton9 <samwalton9@gmail.com>
2019-04-15 14:56:20 +0100

    Add per-organisation user limiting to model

--- extlinks/programs/models.py
+++ extlinks/programs/models.py
@@ -17,6 +17,13 @@ class Organisation(models.Model):
     program = models.ForeignKey(Program, blank=True, null=True,
                                 on_delete=models.SET_NULL)
 
+    # Some organisation use cases will want to limit link change tracking
+    # to a particular list of users.
+    limit_by_user = models.BooleanField(default=False)
+    username_list = models.TextField(blank=True, null=True)
+    # If a URL is placed here, we'll use it to regularly update username_list
+    username_list_url = models.URLField(blank=True, null=True)
+
 
 class Collection(models.Model):
     class Meta:
