9e31dfb348479bb6bf50c347c73d6614d51ac26c
Samwalton9 <samwalton9@gmail.com>
2019-04-30 15:34:28 +0100

    Add example data generation scripts for dev

---
+++ bin/example_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Creating Programs"
+python /app/manage.py programs_example_data 10
+echo "Creating LinkSearchTotals"
+python /app/manage.py linksearchtotal_example_data 60
+python /app/manage.py linkevent_example_data 10000
---
+++ extlinks/links/management/commands/linkevent_example_data.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta, timezone
+import random
+from faker import Faker
+
+from django.core.management import BaseCommand
+
+from ...models import URLPattern, LinkEvent
+
+
+class Command(BaseCommand):
+    help = "Backfills a set of LinkSearchTotals for each url pattern"
+
+    def add_arguments(self, parser):
+        parser.add_argument('num_events', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        # Number of link events to log in total
+        num_events = options['num_events'][0]
+
+        fake = Faker()
+        languages = ['en', 'de', 'fr', 'he', 'hi', 'ta']
+        usernames = [fake.user_name() for _ in range(200)]
+        # Hacky way of adding a weighted random choice of change type.
+        # Addition is likely to be more prevalent.
+        change_choices = [LinkEvent.ADDED, LinkEvent.ADDED,
+                          LinkEvent.ADDED, LinkEvent.REMOVED]
+
+        urlpatterns = URLPattern.objects.all()
+
+        for _ in range(num_events):
+            urlpattern = random.choice(urlpatterns)
+            new_event = LinkEvent(
+                link=urlpattern.url + "/" + fake.word(),
+                timestamp=fake.date_time_between(
+                    start_date=datetime.now()-timedelta(days=365),
+                    end_date="now",
+                    tzinfo=timezone.utc
+                ),
+                domain=random.choice(languages) + ".wikipedia.org",
+                username=random.choice(usernames),
+                rev_id=random.randint(10000000, 100000000),
+                user_id=random.randint(10000000, 100000000),
+                page_title=fake.word(),
+                page_namespace=0,
+                event_id=fake.uuid4(),
+                change=random.choice(change_choices)
+            )
+            new_event.save()
+
+            new_event.url.add(urlpattern)
