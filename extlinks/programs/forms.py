c2e92ca02228cddbbcbe7cc039ea75c746115429
Samwalton9 <samwalton9@gmail.com>
2019-05-01 13:41:33 +0100

    Add simple date range selector on program pages

---
+++ extlinks/programs/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class DateInput(forms.DateInput):
+    # Creating a custom widget because default DateInput doesn't use
+    # input type="date"
+    input_type = 'date'
+
+
+class FilterForm(forms.Form):
+
+    start_date = forms.DateField(required=False, label="Start date:",
+                                 widget=DateInput())
+    end_date = forms.DateField(required=False, label="End date:",
+                               widget=DateInput())
