7ef4819dac615e6af166ba79b04046de7accc514
Sam Walton <samwalton9@gmail.com>
2019-07-02 15:40:20 +0100

    Program not necessary for orgs

--- extlinks/organisations/models.py
+++ extlinks/organisations/models.py
@@ -19,7 +19,7 @@ class Organisation(models.Model):
     name = models.CharField(max_length=40)
 
     # programs.Program syntax required to avoid circular import.
-    program = models.ManyToManyField('programs.Program')
+    program = models.ManyToManyField('programs.Program', blank=True)
 
     username_list = models.ManyToManyField(User, blank=True)
     # If a URL is placed here, we'll use it to regularly update username_list
