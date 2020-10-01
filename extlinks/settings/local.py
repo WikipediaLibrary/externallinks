d032eb0003c8144e7a64a054f7afc1e23356ab40
suecarmol <scardenasmolinar@wikimedia.org>
2020-10-01 12:16:10 -0500

    Add Django Debug Toolbar to local dev environments

--- extlinks/settings/local.py
+++ extlinks/settings/local.py
@@ -5,3 +5,28 @@ DEBUG = True
 
 SERVER_EMAIL = "Wikilink Local <wikilink.local@localhost.localdomain>"
 DEFAULT_FROM_EMAIL = SERVER_EMAIL
+
+
+# Django Debug Toolbar config
+# ------------------------------------------------------------------------------
+
+# Sometimes, developers do not want the debug toolbar on their local environments,
+# so we can disable it by not passing a REQUIREMENTS_FILE variable when building
+# the docker containers
+if os.environ["REQUIREMENTS_FILE"] == "local.txt":
+    INSTALLED_APPS += [
+        "debug_toolbar",
+    ]
+
+    MIDDLEWARE += [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+
+    INTERNAL_IPS = ["127.0.0.1", "localhost", "0.0.0.0"]
+
+    def show_toolbar(request):
+        return True
+
+    DEBUG_TOOLBAR_CONFIG = {
+        "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+    }
--- extlinks/urls.py
+++ extlinks/urls.py
@@ -1,5 +1,7 @@
+import os
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 from extlinks.programs.urls import urlpatterns as programs_urls
 from extlinks.organisations.urls import urlpatterns as organisations_urls
@@ -7,12 +9,19 @@ from extlinks.organisations.urls import urlpatterns as organisations_urls
 from .views import Homepage, Documentation
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', Homepage.as_view(), name='homepage'),
-    path('docs', Documentation.as_view(), name='documentation'),
-
-    path('programs/', include((programs_urls, 'programs'),
-                              namespace='programs')),
-    path('organisations/', include((organisations_urls, 'organisations'),
-                                   namespace='organisations'))
+    path("admin/", admin.site.urls),
+    path("", Homepage.as_view(), name="homepage"),
+    path("docs", Documentation.as_view(), name="documentation"),
+    path("programs/", include((programs_urls, "programs"), namespace="programs")),
+    path(
+        "organisations/",
+        include((organisations_urls, "organisations"), namespace="organisations"),
+    ),
 ]
+
+if settings.DEBUG and os.environ["REQUIREMENTS_FILE"] == "local.txt":
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
