f370278cec30e7d629a9632fc04e3a0c5920f4cf
Jason Sherman <jsnshrmn@users.noreply.github.com>
2020-10-01 13:00:54 -0500

    Apply suggestions from code review
    Co-authored-by: Susana Cárdenas Molinar <7854953+suecarmol@users.noreply.github.com>

--- extlinks/links/management/commands/linkevents_collect.py
+++ extlinks/links/management/commands/linkevents_collect.py
@@ -48,17 +48,17 @@ class Command(BaseCommand):
         # Eventsource should fail if it can't read data after ~15 minutes.
         for event in EventSource(
             url,
-            # The retry argument sets the delay between retries in milliseconds.
-            # We're setting this to 5 minutes.
-            # There's no way to set the max_retries value with this library,
-            # but since it depends upon requests, which in turn uses urllib3
-            # by default, we get a default max_retries value of 3.
+         """The retry argument sets the delay between retries in milliseconds.
+             We're setting this to 5 minutes.
+            There's no way to set the max_retries value with this library,
+            but since it depends upon requests, which in turn uses urllib3
+            by default, we get a default max_retries value of 3."""
             retry=300000,
-            # The timeout argument gets passed to requests.get.
-            # An integer value sets connect (socket connect) and
-            # read (time to first byte / since last byte) timeout values.
-            # A tuple value sets each respective value independently.
-            # https://requests.readthedocs.io/en/latest/user/advanced/#timeouts
+            """The timeout argument gets passed to requests.get.
+            An integer value sets connect (socket connect) and
+            read (time to first byte / since last byte) timeout values.
+            A tuple value sets each respective value independently.
+            https://requests.readthedocs.io/en/latest/user/advanced/#timeouts"""
             timeout=(3.05, 30),
         ):
             if event.event == 'message':
