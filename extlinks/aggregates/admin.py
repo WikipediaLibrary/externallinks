64608719d88d82a6a4736bc93c5a24da71baa0f8
suecarmol <scardenasmolinar@wikimedia.org>
2020-11-04 12:14:04 -0600

    Add filters for LinkAggregateAdmin

--- extlinks/aggregates/admin.py
+++ extlinks/aggregates/admin.py
@@ -5,11 +5,13 @@ from .models import LinkAggregate
 
 class LinkAggregateAdmin(admin.ModelAdmin):
     list_display = (
+        "organisation",
         "collection",
         "full_date",
         "total_links_added",
         "total_links_removed",
     )
+    list_filter = ("organisation", "collection", "month", "year")
 
 
 admin.site.register(LinkAggregate, LinkAggregateAdmin)
