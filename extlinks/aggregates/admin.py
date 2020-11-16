b210ea4be9a5e2ac609916f8c21b9c621adcd3bf
suecarmol <scardenasmolinar@wikimedia.org>
2020-11-16 13:10:26 -0600

    Add admin for UserAggregate

--- extlinks/aggregates/admin.py
+++ extlinks/aggregates/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import LinkAggregate
+from .models import LinkAggregate, UserAggregate
 
 
 class LinkAggregateAdmin(admin.ModelAdmin):
@@ -15,3 +15,18 @@ class LinkAggregateAdmin(admin.ModelAdmin):
 
 
 admin.site.register(LinkAggregate, LinkAggregateAdmin)
+
+
+class UserAggregateAdmin(admin.ModelAdmin):
+    list_display = (
+        "organisation",
+        "collection",
+        "username",
+        "full_date",
+        "total_links_added",
+        "total_links_removed",
+    )
+    list_filter = ("organisation", "collection", "month", "year")
+
+
+admin.site.register(UserAggregate, UserAggregateAdmin)
