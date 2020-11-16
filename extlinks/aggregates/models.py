7dd11cbf97901ad770e53379f7b3c5dc5b9d8a82
suecarmol <scardenasmolinar@wikimedia.org>
2020-11-16 13:08:49 -0600

    Add model and migration for UserAggregate

--- extlinks/aggregates/models.py
+++ extlinks/aggregates/models.py
@@ -41,3 +41,44 @@ class LinkAggregate(models.Model):
                 message="LinkAggregate with this combination (organisation, collection, full_date) already exists.",
                 code="unique_together",
             )
+
+
+class UserAggregate(models.Model):
+    class Meta:
+        app_label = "aggregates"
+
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+    collection = models.ForeignKey(
+        Collection, on_delete=models.CASCADE, blank=False, null=False
+    )
+    username = models.CharField(max_length=235)
+    day = models.PositiveIntegerField()
+    month = models.PositiveIntegerField()
+    year = models.PositiveIntegerField()
+    full_date = models.DateField()
+    total_links_added = models.PositiveIntegerField()
+    total_links_removed = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.day = self.full_date.day
+        self.month = self.full_date.month
+        self.year = self.full_date.year
+        if self.pk is None:
+            self.full_clean(validate_unique=True)
+        super().save(*args, **kwargs)
+
+    def validate_unique(self, *args, **kwargs):
+        super().validate_unique(*args, **kwargs)
+
+        if self.__class__.objects.filter(
+            organisation=self.organisation,
+            collection=self.collection,
+            username=self.username,
+            full_date=self.full_date,
+        ).exists():
+            raise ValidationError(
+                message="UserAggregate with this combination (organisation, collection, username, full_date) already exists.",
+                code="unique_together",
+            )
