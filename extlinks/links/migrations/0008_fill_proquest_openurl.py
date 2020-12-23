b5f3c5cb4a40a8a537a01779952dc913c5f6f53c
suecarmol <scardenasmolinar@wikimedia.org>
2020-12-22 18:32:11 -0600

    Create migration to fix problem with Proquest Open URL

---
+++ extlinks/links/migrations/0008_fill_proquest_openurl.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+
+
+def add_link_events_to_proquest_openurl_collection(apps, schema_editor):
+    LinkEvent = apps.get_model("links", "LinkEvent")
+    Collection = apps.get_model("organisations", "Collection")
+    URLPattern = apps.get_model("links", "URLPattern")
+    proquest_openurl_collection = Collection.objects.filter(name="Proquest OpenURL")
+    if proquest_openurl_collection:
+        proquest_openurl_linkevents = LinkEvent.objects.filter(
+            link__icontains="gateway.proquest.com/openurl"
+        )
+        for proquest_openurl_linkevent in proquest_openurl_linkevents:
+            proquest_openurl_linkevent.url.add(proquest_openurl_collection.url.get())
+            proquest_openurl_linkevent.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("links", "0007_auto_20190730_1355")]
+
+    operations = [migrations.RunPython(add_link_events_to_proquest_openurl_collection)]
