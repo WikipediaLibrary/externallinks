0f66670a8d00d35669974ab5af4079fbdaca0e28
suecarmol <scardenasmolinar@wikimedia.org>
2021-12-14 20:39:33 -0600

    Add filter fields in organizations and collections admin

--- extlinks/organisations/admin.py
+++ extlinks/organisations/admin.py
@@ -4,21 +4,23 @@ from .models import Organisation, Collection, User
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username',)
+    list_display = ("username",)
 
 
 admin.site.register(User, UserAdmin)
 
 
 class OrganisationAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ("name",)
+    list_filter = ("name",)
 
 
 admin.site.register(Organisation, OrganisationAdmin)
 
 
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'organisation')
+    list_display = ("name", "organisation")
+    list_filter = ("name", "organisation")
 
 
 admin.site.register(Collection, CollectionAdmin)
