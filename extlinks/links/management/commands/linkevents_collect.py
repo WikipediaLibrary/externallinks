1006be886c9aa942b2dc27107b5a46e71b2caf82
Jason Sherman <jsnshrmn@users.noreply.github.com>
2022-01-19 09:33:35 -0600

    HOTFIX: linkevents_collect bug

--- extlinks/links/management/commands/linkevents_collect.py
+++ extlinks/links/management/commands/linkevents_collect.py
@@ -157,7 +157,9 @@ class Command(BaseCommand):
         # I can't think of any situation when this wouldn't be the
         # case, but I can't wait to find out why I'm wrong.
         on_user_list = False
-        this_link_org = url_patterns[0].collection.organisation
+        this_link_collection = url_patterns[0].collection
+        if hasattr(this_link_collection, "organization"):
+            this_link_org = url_patterns[0].collection.organisation
         if hasattr(this_link_org, "username_list"):
             username_list = this_link_org.username_list
             if username_list:
