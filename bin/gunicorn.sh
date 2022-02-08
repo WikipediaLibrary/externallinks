10d8d22561dc1dbbb8eb77d2cdb8de18d69bd321
jsn <jsherman@wikimedia.org>
2022-02-08 12:32:44 -0600

    HOTFIX: more gunicorn workers, limit another bot

--- bin/gunicorn.sh
+++ bin/gunicorn.sh
@@ -6,7 +6,7 @@ python manage.py collectstatic --noinput
 exec gunicorn extlinks.wsgi:application \
     --name extlinks_django \
     --bind 0.0.0.0:8000 \
-    --workers 1 \
+    --workers 3 \
     --timeout 300 \
     --backlog 2048 \
     --log-level=info \
