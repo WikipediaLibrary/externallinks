8842c5ccebb240ed76e83da2c6f4b6c6d7a79bd8
jsn <jsherman@wikimedia.org>
2024-08-16 13:03:29 -0500

    capture migration work

--- bin/clean.sh
+++ bin/clean.sh
@@ -16,20 +16,20 @@ then
         exit
     fi
     mysql_cmd="mysql -h db -u root -p${MYSQL_ROOT_PASSWORD} -D ${MYSQL_DATABASE}"
-    concat_fragment="GROUP_CONCAT('DROP TABLE IF EXISTS ', table_name, ';')"
+    # Build an SQL statement for dropping every table
+    concat_fragment="GROUP_CONCAT('DROP TABLE IF EXISTS ', table_name SEPARATOR ';')"
     get_tables_query="SELECT ${concat_fragment} FROM information_schema.tables WHERE table_schema = '${MYSQL_DATABASE}';"
     drop_query=$(echo ${get_tables_query} | ${mysql_cmd})
     drop_query=${drop_query/$concat_fragment/}
-    drop_query=${drop_query/\n/}
-    if [ $drop_query = "NULL" ]
+    drop_query=${drop_query//[$'\r\n']}
+    if [ "$drop_query" == "NULL" ]
     then
         echo "No tables to drop."
         exit
     fi
-    echo "\"$drop_query"\"
     drop_query="SET FOREIGN_KEY_CHECKS = 0;${drop_query};SET FOREIGN_KEY_CHECKS = 1;"
-    echo ${drop_query}
     echo "Dropping tables."
+    echo ${drop_query}
     nice -n 5 bash -c "echo \"${drop_query}\" | ${mysql_cmd}"
 
     echo "Tables dropped."
