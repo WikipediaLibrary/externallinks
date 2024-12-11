841124b609f6dac50c0b89f031e1054a8853af56
Kgraessle <kgraessle@wikimedia.org>
2024-12-11 17:08:07 -0600

    This moves the django admin relationship display from LinkEvents to URLPatterns
    - See https://docs.djangoproject.com/en/5.1/ref/contrib/admin/#using-generic-relations-as-an-inline for documentation on generic relationships displaying in django admin
    
    Bug: T370901

--- extlinks/links/admin.py
+++ extlinks/links/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline, GenericStackedInline
 
 from .models import URLPattern, LinkSearchTotal, LinkEvent
 
 
+class LinkEventURLPatternAdminInline(GenericTabularInline):
+    model = LinkEvent
+
+
 class URLPatternAdmin(admin.ModelAdmin):
     list_display = ("url",)
+    inlines = [
+        LinkEventURLPatternAdminInline,
+    ]
 
 
 admin.site.register(URLPattern, URLPatternAdmin)
@@ -19,6 +27,12 @@ admin.site.register(LinkSearchTotal, LinkSearchTotalAdmin)
 
 class LinkEventAdmin(admin.ModelAdmin):
     list_display = ("link", "timestamp", "domain", "username", "change")
+    readonly_fields = ["url_pattern_display"]
+    exclude = ["content_type", "object_id"]
+
+    @admin.display(description="URLPattern")
+    def url_pattern_display(self, instance):
+        return URLPattern.objects.filter(pk=instance.content_object.pk).first()
 
 
 admin.site.register(LinkEvent, LinkEventAdmin)
