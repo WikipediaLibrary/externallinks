e713dd5b2b25faa36a0828a8014d19ac169fbda4
mimurawil <mimurawil@gmail.com>
2025-02-26 16:38:49 -0800

    Set UI filter dates to consider entire month

--- extlinks/common/tests.py
+++ extlinks/common/tests.py
@@ -2,14 +2,11 @@ from datetime import date, datetime, timezone
 import time_machine
 from unittest import mock
 
-from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from extlinks.common.forms import FilterForm, YearMonthField
-from extlinks.common.helpers import (
-    get_linksearchtotal_data_by_time,
-    get_normalized_date_for_display,
-)
+from extlinks.common.forms import FilterForm
+from extlinks.common.helpers import get_linksearchtotal_data_by_time
+
 from extlinks.links.factories import LinkSearchTotalFactory, URLPatternFactory
 from extlinks.links.models import LinkSearchTotal
 
@@ -44,45 +41,14 @@ class LinkSearchDataByTimeTest(TestCase):
             self.assertEqual(12, len(dates))
             self.assertEqual(12, len(linksearch_data))
 
-    def test_get_normalized_date_for_display(self):
-        self.assertEqual("2025-02", get_normalized_date_for_display("2025-02-01"))
-        self.assertEqual("2025-01", get_normalized_date_for_display("2025-01"))
-        self.assertEqual("", get_normalized_date_for_display("invalid date"))
-
-
-class YearMonthFieldTest(TestCase):
-    def setUp(self):
-        self.field = YearMonthField(required=False)
-
-    def test_valid_year_month(self):
-        self.assertEqual(self.field.clean("2025-02"), date(2025, 2, 1))
-        self.assertEqual(self.field.clean("1999-12"), date(1999, 12, 1))
-
-    def test_invalid_format(self):
-        with self.assertRaisesMessage(ValidationError, "Enter a valid year-month"):
-            self.field.clean("2025/02")
-
-        with self.assertRaisesMessage(ValidationError, "Enter a valid year-month"):
-            self.field.clean("abcd-12")
-
-        with self.assertRaisesMessage(ValidationError, "Enter a valid year-month"):
-            self.field.clean("2025-13")
-
-        with self.assertRaisesMessage(ValidationError, "Enter a valid year-month"):
-            self.field.clean("2025")
-
-    def test_empty_value(self):
-        self.assertIsNone(self.field.clean(""))
-        self.assertIsNone(self.field.clean(None))
-
 
 class FilterFormTest(TestCase):
 
     def test_valid_data(self):
         form = FilterForm(
             data={
-                "start_date": "2025-02",
-                "end_date": "2025-06",
+                "start_date": "2025-02-15",
+                "end_date": "2025-06-21",
                 "limit_to_user_list": "on",
                 "namespace_id": "10",
                 # "exclude_bots": "on", # omit this key to assertFalse
@@ -107,26 +73,31 @@ class FilterFormTest(TestCase):
         self.assertFalse(form.cleaned_data["exclude_bots"])
 
     def test_invalid_start_date(self):
-        form = FilterForm(data={"start_date": "2025/02"})
+        form = FilterForm(data={"start_date": "2025/02/01"})
         self.assertFalse(form.is_valid())
         self.assertIn("start_date", form.errors)
 
+    def test_clean_start_date(self):
+        form = FilterForm(data={"start_date": "2023-11-11"})
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["start_date"], date(2023, 11, 1))
+
     def test_invalid_end_date(self):
-        form = FilterForm(data={"end_date": "abcd-12"})
+        form = FilterForm(data={"end_date": "abcd-12-ef"})
         self.assertFalse(form.is_valid())
         self.assertIn("end_date", form.errors)
 
     def test_clean_end_date(self):
-        form = FilterForm(data={"end_date": "2023-11"})
+        form = FilterForm(data={"end_date": "2023-11-01"})
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["end_date"], date(2023, 11, 30))
 
     def test_clean_end_date_leap_year(self):
-        form = FilterForm(data={"end_date": "2024-02"})
+        form = FilterForm(data={"end_date": "2024-02-01"})
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["end_date"], date(2024, 2, 29))
 
     def test_clean_end_date_feb_non_leap_year(self):
-        form = FilterForm(data={"end_date": "2025-02"})
+        form = FilterForm(data={"end_date": "2025-02-01"})
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["end_date"], date(2025, 2, 28))
