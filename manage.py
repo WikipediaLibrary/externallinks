212fd34d8f5b6e82d05d48059b2c97be5c084771
jsnshrmn <2986893+jsnshrmn@users.noreply.github.com>
2025-03-31 15:13:54 -0500

    Replace django-cron with crontab
    - move commands defined in django-cron classes to a crontab file
    - load environment variables via dotenv since crontab doesn't pass env
    - migrate backup script to python for reliable env loading
    
    See: https://phabricator.wikimedia.org/T370977
    
    Bug: T370977

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
@@ -13,6 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    load_dotenv(".env")
     execute_from_command_line(sys.argv)
 
 
